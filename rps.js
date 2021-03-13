1d15be5f2268d505ea126bd4762ca980ab70893f
Alisha <alisha_56789@yahoo.com>
2021-03-12 19:19:48 -0700

    completed rock paper scissors project. comments throughout

---
+++ rps.js
@@ -0,0 +1,12 @@
+if (playerScore <= 5 || computerScore <= 5);
+
+/*when I use this, the program will continue until both players have a score of OVER 5 and 
+then it will display a message*/
+
+function isEmpty(str){
+    if (str == '') {
+        return true;
+    } else {
+        return false;
+    }
+}
